e0ed8ee8faedf456905bb7309f3034e6b322add5
Abel2213 <TAE2024.8@cinvestav.mx>
2024-04-27 02:15:34 -0600

    Sin comentarios

--- source/test.c
+++ source/test.c
@@ -10,12 +10,8 @@
  * @brief   Application entry point.
  */
 #include <stdio.h>
-#include "board.h"
-#include "peripherals.h"
-#include "pin_mux.h"
-#include "clock_config.h"
+#include <stdint.h>
 #include "MK64F12.h"
-#include "fsl_debug_console.h"
 /* TODO: insert other include files here. */
 
 /* TODO: insert other definitions and declarations here. */
@@ -24,26 +20,16 @@
  * @brief   Application entry point.
  */
 int main(void) {
+	SIM->SCGC5 = 0x400;
+	PORTB->PCR[22] = 0x00000100;
+	GPIOB->PDOR= 0x00400000;
+	GPIOB->PDOR= 0x00400000;
 
-    /* Init board hardware. */
-    BOARD_InitBootPins();
-    BOARD_InitBootClocks();
-    BOARD_InitBootPeripherals();
-#ifndef BOARD_INIT_DEBUG_CONSOLE_PERIPHERAL
-    /* Init FSL debug console. */
-    BOARD_InitDebugConsole();
-#endif
-
-    PRINTF("Hello World\r\n");
-
-    /* Force the counter to be placed into memory. */
-    volatile static int i = 0 ;
-    /* Enter an infinite loop, just incrementing a counter. */
-    while(1) {
-        i++ ;
-        /* 'Dummy' NOP to allow source level single stepping of
-            tight while() loop */
-        __asm volatile ("nop");
-    }
-    return 0 ;
+	while(1){
+		GPIOB->PDOR= 0x00400000;
+		GPIOB->PDOR= 0x00400000;
+		GPIOB->PDOR= 0x00200000;
+		printf("RED LED OFF\n");
+	}
+        return 0 ;
 }
